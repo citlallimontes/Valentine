02c33ca0cb6bf6f07cea5c43c7c7ced97717a8f2
citlallimontes <99051488+citlallimontes@users.noreply.github.com>
2022-02-09 13:27:57 -0800

    Add files via upload

--- java.js
+++ java.js
@@ -1,14 +1,14 @@
-function joy(){
-  document.getElementById("reply").innerHTML = "fuck yeah! I'm so glad you said yes. text... :) !"
-  document.getElementById("reply").style.visibility = "visible";
+function joy() {
+	document.getElementById("reply").innerHTML = "fuck yeah! I'm so glad you said yes. text... :) !";
+	document.getElementById("reply").style.visibility = "visible";
 }
 
-function despair(){
-  document.getElementById("reply").innerHTML = "Oh that's okay, no worries! text... :( "
-  document.getElementById("reply").style.visibility = "visible";
+function despair() {
+	document.getElementById("reply").innerHTML = "Oh that's okay, no worries! text... :( ";
+	document.getElementById("reply").style.visibility = "visible";
 }
 
-function consider(){
-  document.getElementById("reply").innerHTML = "I'll give you some time to think about it! text... :/"
-  document.getElementById("reply").style.visibility = "visible";
+function consider() {
+	document.getElementById("reply").innerHTML = "I'll give you some time to think about it! text... :/";
+	document.getElementById("reply").style.visibility = "visible";
 }
